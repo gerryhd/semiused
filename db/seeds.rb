603afb65d848dc2775a5a14afe362620d97d225c
Gerardo Hernandez <gerardo@coatilabs.com>
2021-05-07 00:55:00 -0500

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email: "admin@example.com", password: "adminadmin", password_confirmation: "adminadmin", role: "admin")
+
+dealership = Dealership.create(name: "San Andreas Cars", location: "50th St. Los Santos")
+dealership_two = Dealership.create(name: "Freedom Cars", location: "Liberty City")
+
+kuruma = Car.create(name: "Kuruma", release_date: 1.year.ago, original_price: 1000000)
+kuruma.car_listings.create(dealership: dealership)
+kuruma.car_listings.create(dealership: dealership_two)
+
+Car.create(name: "Coquette", release_date: 4.months.ago, original_price: 800000).car_listings.create(dealership: dealership)
